900ad79abf4e37d0da5949f9853906070d7c85c0
Hamzayajjou <hamzayajjou78@gmail.com>
2025-10-01 16:24:53 +0200

    Add files via upload

---
+++ ap2java_sql/src/ap2java_sql/LIVRE.java
@@ -0,0 +1,52 @@
+package ap2java_sql;
+
+
+public class LIVRE {
+	private String ISBN;
+	private String titre;
+	private float prix;
+	private AUTEUR Auteur;
+	private ADHERENT Emprunteur;
+	
+	public LIVRE(String iSBN, String titre, float prix, AUTEUR auteur, ADHERENT emprunteur) {
+		super();
+		ISBN = iSBN;
+		this.titre = titre;
+		this.prix = prix;
+		Auteur = auteur;
+		Emprunteur = emprunteur;
+	}
+	public ADHERENT getEmprunteur() {
+		return Emprunteur;
+	}
+	public void setEmprunteur(ADHERENT emprunteur) {
+		Emprunteur = emprunteur;
+	}
+
+	public String getISBN() {
+		return ISBN;
+	}
+	public void setISBN(String iSBN) {
+		ISBN = iSBN;
+	}
+	public String getTitre() {
+		return titre;
+	}
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+	public float getPrix() {
+		return prix;
+	}
+	public void setPrix(float prix) {
+		this.prix = prix;
+	}
+	public AUTEUR getAuteur() {
+		return Auteur;
+	}
+	public void setAuteur(AUTEUR auteur) {
+		Auteur = auteur;
+	}
+	
+
+}
---
+++ ap2java_sql/src/module-info.java
@@ -0,0 +1,4 @@
+module ap2java_sql {
+    requires java.sql; // car tu utilises JDBC
+    exports ap2java_sql;
+}
